0158bcf81a27a024ceb8307020a134c21fba6d9f
BrianArchibald <brianallenarchibald@gmail.com>
2018-03-28 20:03:06 -0500

    arrow functions

--- ES6.js
+++ ES6.js
@@ -97,3 +97,57 @@ librarySystem('sandwichLibray', function () {
 	console.log(sandwichLibrary);
 })(); //object {breads: Object, fillings: Object}
 
+
+////////Arrow Functions//////////////////////////
+
+// ES5
+var multiplyES5 = function(x, y) {
+  return x * y;
+};
+
+// ES6
+const multiplyES6 = (x, y) => { return x * y };
+
+const multiplyES6 = (x, y) => x * y;
+
+//ES5
+var phraseSplitterEs5 = function phraseSplitter(phrase) {
+  return phrase.split(' ');
+};
+
+//ES6
+const phraseSplitterEs6 = phrase => phrase.split(" ");
+
+console.log(phraseSplitterEs6("ES6 Awesomeness"));  // ["ES6", "Awesomeness"]
+
+//ES5
+var setNameIdsEs5 = function setNameIds(id, name) {
+  return {
+    id: id,
+    name: name
+  };
+};
+
+// ES6
+var setNameIdsEs6 = (id, name) => ({ id: id, name: name });
+
+console.log(setNameIdsEs6 (4, "Kyle"));   // Object {id: 4, name: "Kyle"}
+
+const smartPhones = [
+  { name:'iphone', price:649 },
+  { name:'Galaxy S6', price:576 },
+  { name:'Galaxy Note 5', price:489 }
+];
+We could create an array of objects with just the names or prices by doing this in ES5:
+
+// ES5
+var prices = smartPhones.map(function(smartPhone) {
+  return smartPhone.price;
+});
+
+console.log(prices); // [649, 576, 489]
+An arrow function is more concise and easier to read:
+
+// ES6
+const prices = smartPhones.map(smartPhone => smartPhone.price);
+console.log(prices); // [649, 576, 489]
